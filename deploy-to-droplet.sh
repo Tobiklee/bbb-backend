f1b9105e1d07a884413962bbbdb036be7c3f59df
Tobiklee <tobiaskleemann1995@gmail.com>
2021-01-09 18:44:17 +0100

    adjust arrow length

--- deploy-to-droplet.sh
+++ deploy-to-droplet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # connect to remote droplet and deploy
-echo "--------------------> connect to droplet and deploy" 
+echo "--------------> connect to droplet and deploy" 
 ssh -i ~/.ssh/id_rsa root@207.154.243.130 'sh deploy.sh'
