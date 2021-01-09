802efa8a48735a83a56287c0ebb498a9463809ec
Tobiklee <tobiaskleemann1995@gmail.com>
2021-01-09 18:36:01 +0100

    finalize deployment with docker-compose

--- deploy-to-droplet.sh
+++ deploy-to-droplet.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-# build docker image locally
-echo "--------------------> build docker image locally"
-sudo docker build . -t bbb-backend
-
-# tag docker image
-echo "--------------------> tag docker image"
-sudo docker tag bbb-backend tobiask95/bbb-backend
-
-# push to remote repository (make sure to docker login first)
-echo "--------------------> push to remote repository"
-sudo docker push tobiask95/bbb-backend
-
-# connect to remote droplet and run docker image
+# connect to remote droplet and deploy
 echo "--------------------> connect to droplet and deploy" 
 ssh -i ~/.ssh/id_rsa root@207.154.243.130 'sh deploy.sh'
